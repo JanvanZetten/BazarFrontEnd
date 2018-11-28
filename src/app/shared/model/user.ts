f5635c5f1452c1d379ed7e127aa5b8c267ca1589
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-11-28 13:45:40 +0100

    Added Booth entity and booth service

---
+++ src/app/shared/model/user.ts
@@ -0,0 +1,4 @@
+export class User{
+  id: number;
+  //TODO add The Users Information it should be like the user in the backend without the password specific stuff
+}
---
+++ src/app/shared/services/booth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {LoginService} from "./login.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {Booth} from "../model/booth";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoothService {
+  Url = environment.apiUrl + '/booths';
+
+  constructor(private http: HttpClient, private authenticationService: LoginService) { }
+
+  private setOptions() {
+    httpOptions.headers =
+      httpOptions.headers.set('Authorization', 'Bearer ' + this.authenticationService.getToken());
+  }
+
+  getBooths(): Observable<Booth[]> {
+    this.setOptions();
+    return this.http.get<Booth[]>(this.Url, httpOptions);
+  }
+
+  getBooth(id: number): Observable<Booth>{
+    this.setOptions();
+    return this.http.get<Booth>(this.Url + "/" + id, httpOptions);
+  }
+
+  addBooth(booth: Booth): Observable<Booth>{
+    this.setOptions();
+    return this.http.get<Booth>(this.Url, booth, httpOptions);
+  }
+
+  updateBooth(booth: Booth): Observable<Booth> {
+    this.setOptions();
+    return this.http.put<Booth>(this.Url + '/' + booth.id, booth, httpOptions);
+  }
+
+  deleteBooth(id: number): Observable<Booth> {
+    this.setOptions();
+    return this.http.delete<Booth>(this.Url + '/' + id, httpOptions);
+  }
+}
