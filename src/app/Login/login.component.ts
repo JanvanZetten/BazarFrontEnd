c5fbc37bd9e7cb2f8cb2f17e08bcc1fef8c6abb7
Husso1997 <hussain1961@hotmail.com>
2018-11-28 12:44:08 +0100

    made some fixezzz

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {LoginService} from '../shared/services/login.service';
 import {Router} from '@angular/router';
 
