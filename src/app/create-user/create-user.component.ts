2efaec2822aee5d789916436783883d291004137
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-12-06 12:41:35 +0100

    added some error handling

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {RegisterService} from "../shared/services/register.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-user',
@@ -16,14 +17,31 @@ export class CreateUserComponent implements OnInit {
   error = false;
   errorMessage: String;
 
-  constructor(private registerService: RegisterService) { }
+  constructor(private registerService: RegisterService, private router: Router) { }
 
   ngOnInit() {
   }
 
   AddLogin() {
     if (this.addUserLoginForm.get('password').value === this.addUserLoginForm.get('passwordRepeated').value){
-      this.registerService.newUser(this.addUserLoginForm.get('username').value, this.addUserLoginForm.get('password').value);
+      if (this.addUserLoginForm.get('password').value !== null && this.addUserLoginForm.get('username').value !== null) {
+        this.registerService.newUser(this.addUserLoginForm.get('username').value, this.addUserLoginForm.get('password').value)
+          .subscribe(() => {
+              this.router.navigate(['/']);
+            },
+            error => {
+              this.errorMessage = error.error, this.error = true,
+                console.log(error);
+            });
+      }
+      else {
+        this.errorMessage = "Der skal være både brugernavn og kodeord";
+        this.error = true;
+      }
+    }
+    else{
+      this.errorMessage = "De to kodeord er ikke ens";
+      this.error = true;
     }
 
   }
