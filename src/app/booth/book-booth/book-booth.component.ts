3ab2ed9394cdb0af9567ee5e29e8f1c854666d51
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-12-11 15:10:29 +0100

    Geting the data from the backend and added alerts

--- src/app/booth/book-booth/book-booth.component.ts
+++ src/app/booth/book-booth/book-booth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {BoothService} from '../../shared/services/booth.service';
 import {LoginService} from '../../shared/services/login.service';
 import {Booth} from '../../shared/model/booth';
+import {AlertComponent} from "ngx-bootstrap";
 
 @Component({
   selector: 'app-book-booth',
@@ -9,13 +10,21 @@ import {Booth} from '../../shared/model/booth';
   styleUrls: ['./book-booth.component.css']
 })
 export class BookBoothComponent implements OnInit {
-booth: Booth;
-booths: Booth[];
-boothsToBook: Booth[];
-error = false;
-success = false;
-errorMessage: String;
-numberOfNonbookedBooths: number;
+  booth: Booth;
+  booths: Booth[];
+  boothsToBook: Booth[];
+  error = false;
+  success = false;
+  errorMessage: String;
+  numberOfNonbookedBooths: number;
+
+  alerts: any[] = [{
+    class: "",
+    type: "",
+    msgStrong: "",
+    msg: "",
+    timeout: 1
+  }]; // Array with descriped anonymous alert object.
 
 
   constructor(private boothService: BoothService, private loginService: LoginService) { }
@@ -24,18 +33,26 @@ numberOfNonbookedBooths: number;
     this.boothService.getAvalibleBoothsCount().subscribe(count => this.numberOfNonbookedBooths = count);
     this.booths = [];
     this.boothsToBook = [];
-    for (var _i = 1; _i < 20; _i++) {
-      let booth = new Booth();
-      booth.id = _i;
-      this.booths.push(booth);
-    }
+
+    this.boothService.getAvalibleBooths().subscribe(
+      bs => this.booths = bs,
+      error => this.alerts.push({
+        class: "text-center",
+        type: "danger",
+        msgStrong: "Fejl!",
+        msg: error.error,
+        timeout: 5000
+      }));
+
   }
 
+  /* This should be deleted when ready but for testing this is still here.
   BookBooth() {
     this.boothService.bookBooth(this.loginService.getUsername(), this.loginService.getToken()).
     subscribe(booth => {this.booth = booth, this.success = true; },
         error => {this.error = true, this.errorMessage = error.error});
   }
+  */
 
   addToBeBooked(booth: Booth) {
     this.booths = this.booths.filter(b => b !== booth);
@@ -45,5 +62,13 @@ numberOfNonbookedBooths: number;
   RemoveFromtobeBooked(booth: Booth) {
     this.boothsToBook = this.boothsToBook.filter(b => b !== booth);
     this.booths.push(booth);
+  }
+
+  /**
+   * Removes alert from alert array.
+   * @param dismissedAlert The alert wanted removed.
+   */
+  onAlertClosed(dismissedAlert: AlertComponent): void {
+    this.alerts = this.alerts.filter(alert => alert !== dismissedAlert);
   }
 }
