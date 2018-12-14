19fc5eac1ed93fe1d5e4368021a3d9b97c1675e2
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-12-14 12:54:01 +0100

    Reset Service

---
+++ src/app/shared/services/reset.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {LoginService} from "./login.service";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResetService {
+
+  Url = environment.apiUrl + '/reset';
+
+  constructor(private http: HttpClient, private authenticationService: LoginService) { }
+
+  private setOptions() {
+    httpOptions.headers =
+      httpOptions.headers.set('Authorization', 'Bearer ' + this.authenticationService.getToken());
+  }
+
+  resetSystem(): Observable<String>{
+    this.setOptions();
+    return this.http.put<String>(this.Url, "\"" + this.authenticationService.getToken() + "\"", httpOptions);
+  }
+}
